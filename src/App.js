6f9caaad787b8d04184abca2e854bd0ba898a810
ZAC <punsalang.zachary@gmail.com>
2020-10-06 20:54:46 -0700

    classes/ componenets (reusable)/

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; //want to import a react library from npm module
 import './App.css';
+import HelloWorld from './HelloWorld';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return (                      //reusable, modifiiable components!
+  <div className="App">
+    <HelloWorld name="Jim"/>    
+    <HelloWorld name="Sally"/>
+  </div>);
+
+};
 
 export default App;
